1454cd392351888d03e220c493b917c0d77297c2
Agustín Beníteez <128295634+AgustinBeniteez@users.noreply.github.com>
2025-05-07 18:47:26 +0200

    v 0.0.2.0

--- src/js/song-list-generator.js
+++ src/js/song-list-generator.js
@@ -1,8 +1,7 @@
-//-------- LEECTURA DE JSON Y MANEJO DE EVENTOS --------//
-
 // Variable global para almacenar la canción seleccionada
 let selectedSong = null;
 
+//-------- LEECTURA DE JSON Y MANEJO DE EVENTOS --------//
 fetch("/src/data/data.json")
   .then((response) => response.json())
   .then((data) => {
@@ -13,16 +12,29 @@ fetch("/src/data/data.json")
       const songElement = document.createElement("div");
       songElement.className = "song";
       songElement.id = `song-${song.title}`;
+
       songElement.innerHTML = `
           <img src="${song.albumImageUrl}" alt="${song.title}">
           <div class="song-details">
             <h2>${song.title}</h2>
-            <p><i class="fa-solid fa-microphone-lines" style="padding-left:2px;"></i> ${song.artist}</p>
+            <p><i class="fa-solid fa-microphone-lines" style="padding-left:2px;"></i> ${
+              song.artist
+            }</p>
             <p><i class="fa-solid fa-compact-disc"></i> ${song.album}</p>
+          </div>
+          <div class="song-duration">
+            <p><i class="fa-solid fa-clock"></i> ${
+              song.duration ? song.duration : "0:00"
+            }</p>
+          </div>
+          <div class="song-releaseYear">
+            <p> <i class="fa-solid fa-calendar-days"></i> ${
+              song.releaseYear
+            }</p>
           </div>
         `;
 
-      //-------- funciones ------------
+      //⭕-------- funciones ------------
       //--- cambia la infomacion del reproductor
       function changeSongInfo(song) {
         //---- obtengo los elementos del html
@@ -40,16 +52,14 @@ fetch("/src/data/data.json")
         infoSongArtist[0].textContent = song.artist;
         lyricsContainer.textContent = song.lyrics;
       }
-
+      //⭕-------- Listener ------------
       // agrega event listener para el clic
       songElement.addEventListener("click", () => {
         selectedSong = song;
-        //---- prueba de cancion selecionada para ver en consola
-        console.log("Canción seleccionada:", selectedSong);
         //---------- llamar a funcion que cambie la cancion del reproductor ------
         changeSongInfo(selectedSong);
       });
-
+      // agrega el elemento a la lista de canciones
       songList.appendChild(songElement);
     });
   });
